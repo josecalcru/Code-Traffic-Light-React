0272f73f3b0bfb6feda4314c269b37b6419d3cb9
josecalcru <josecalcru@gmail.com>
2021-03-04 02:40:22 +0000

    initial upload

---
+++ src/js/component/trafficLight.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+//create your first component
+export function TrafficLight() {
+	return (
+		<div className="container">
+			<div className="color red"> 1</div>
+			<div className="color yellow"> 1</div>
+			<div className="color green">1 </div>
+		</div>
+	);
+}
